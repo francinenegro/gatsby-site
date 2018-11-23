ea4d2f4b8cae84ece5c996f8e0f38b47ae28e4ee
Francesca Negro <francesca.negro@tesglobal.com>
2018-11-23 10:56:31 +0000

    fixed font-import

--- src/pages/alidays-style.js
+++ src/pages/alidays-style.js
@@ -17,7 +17,7 @@ const ProjectPage = () => (
     <p>Samples of all the assets I drew up and delivered to the developer in order to help him in his work.
 Below there are a few pages off the style guide with typography and swatches samples, a thorough flow (much needed in a case like this with many different features to keep things straight during development) and pages off the detailed guide for every view in the app (pixels, colors, spaces and so forth). It took time to develop and put together but became the reference guide for the developer and a primary tool of documentation for the app.</p>
 <div className="Project-Body_small_image">
-    <img src="../images/alidays-style.jpg"></img>
+    <img src="../images/App -guidelines.png"></img>
   </div>
     </div>
   </Layout>
