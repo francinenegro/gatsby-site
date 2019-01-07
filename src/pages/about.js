2b4065e4a5d1209141f1bee08f9b14c6b389fff0
Francesca Negro <francesca.negro@tesglobal.com>
2019-01-07 16:34:22 +0100

    updated +fixed responsivity of about page, added link and updated color

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,23 +5,23 @@ import '../style/project-page.css'
 
 const AboutPage = () => (
   <Layout>
-          <div className="HeroGroup_about">
+          <div className="About_container"> 
           <img src="/images/portrait.JPEG"></img>
-          <div><p>Problem solver, no matter the problem/<br></br>
-I have extensive experience in understanding and translating what people express, so that it becomes a clear directed instruction we can then work on/<br></br>
-I strive to make things easier for the user, and believe that user testing and feedbacks are both a cure and a curse/<br></br>
-I have a degree in Psychology then pivoted towards design/<br></br>
-I am an aesthete/<br></br>
-I love video games and learning how to actually make them/<br></br>
-I am a nerd who will spend countless hours learning new skills and will look up everything I don't know in the process/<br></br>
-I am a perfectionist/<br></br>
-I am a Lead Gift Evangelist, which means you will never have to pick a gift for someone ever again/<br></br>
-I draw cute things I sometimes put into my designs/​​​​​​​<br></br>
-I illustrate (stickers anyone?) and write for JAM London to make the product design conference a reality/</p></div>
+          <div className="About_text">
+          <h2>About me</h2>
+          <h3>Francesca N.</h3>
+            <p>Problem solver, no matter the problem/<br></br><br></br>
+I have extensive experience in understanding and translating what people express, so that it becomes a clear directed instruction we can then work on/<br></br><br></br>
+I strive to make things easier for the user, and believe that user testing and feedbacks are both a cure and a curse/<br></br><br></br>
+I have a degree in Psychology then pivoted towards design/<br></br><br></br>
+I love video games and learning how to actually make them/<br></br><br></br>
+I am a nerd who will spend countless hours learning new skills and will look up everything I don't know in the process/<br></br><br></br>
+I am a Lead Gift Evangelist, which means you will never have to pick a gift for someone ever again/<br></br><br></br> 
+I draw cute things I sometimes put into my designs/​​​​​​​<br></br><br></br>
+I illustrate (stickers anyone?) and write for <a href="https://www.jamlondon.io/" target="blank">JAM London</a> to make the product design conference a reality/</p>
+</div>
         </div>
   </Layout>
 )
-
-
 
 export default AboutPage
