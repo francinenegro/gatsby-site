c6dfead6bd9749c5bb99c50b2ed411a4b084f465
Francesca Negro <francesca.negro@tesglobal.com>
2019-01-07 14:39:59 +0100

    added analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,25 @@ module.exports = {
               pathToConfigModule: `src/utils/typography`,
             },
           },
-  ],
+
+      {resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-77033915-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
+  ]
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,12 +5,9 @@ import '../style/project-page.css'
 
 const AboutPage = () => (
   <Layout>
- <div className="Hero_about"> 
           <div className="HeroGroup_about">
-        </div>
-        </div>
-    <div className="Project-Body">
-    <p>Problem solver, no matter the problem/<br></br>
+          <img src="/images/portrait.JPEG"></img>
+          <div><p>Problem solver, no matter the problem/<br></br>
 I have extensive experience in understanding and translating what people express, so that it becomes a clear directed instruction we can then work on/<br></br>
 I strive to make things easier for the user, and believe that user testing and feedbacks are both a cure and a curse/<br></br>
 I have a degree in Psychology then pivoted towards design/<br></br>
@@ -20,8 +17,8 @@ I am a nerd who will spend countless hours learning new skills and will look up 
 I am a perfectionist/<br></br>
 I am a Lead Gift Evangelist, which means you will never have to pick a gift for someone ever again/<br></br>
 I draw cute things I sometimes put into my designs/​​​​​​​<br></br>
-I illustrate (stickers anyone?) and write for JAM London to make the product design conference a reality/</p>
-    </div>
+I illustrate (stickers anyone?) and write for JAM London to make the product design conference a reality/</p></div>
+        </div>
   </Layout>
 )
 
