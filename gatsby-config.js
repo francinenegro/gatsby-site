1bb6d7b7c1b72deb69c8cece1ec4f62d94881209
Francesca Negro <francesca@liquid.auto>
2019-10-20 16:19:04 +0100

    removed contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,15 +15,7 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/favicomyLogo-01.png', // This path is relative to the root of the site.
       },
-    },
-    // 'gatsby-plugin-offline',
-    // {
-    //   resolve: 'gatsby-source-contentful',
-    //   options: {
-    //     spaceId: 'agjl59xvy97w',
-    //     accessToken: '534fc346a30aab5f7a5ee62aed3aa3a1395ceb69928c0d3843efe17bb8e52cb0'
-    //   }
-    // },
+    }, 
     
      {
             resolve: `gatsby-plugin-typography`,
