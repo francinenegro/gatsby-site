a1255f9569880115d988f9506272bb990b2ba2f2
Francesca Negro <francesca@liquid.auto>
2019-10-28 09:53:16 +0000

    updated pages to fix broken style

--- src/pages/alidays-fast.js
+++ src/pages/alidays-fast.js
@@ -12,25 +12,28 @@ const ProjectPage = () => (
       rel="stylesheet">
       </link>
     <a href="/"><i class="material-icons md-48" >arrow_back</i></a>
-            <h2>Fast Booking</h2>
-            <h3>Designed for <a href="http://www.alidays.it/" target="_blank">Alidays</a>, a travel company always striving to improve their experience through technology.</h3>
+      <h2>Fast Booking</h2>
+      <h3>Designed for <a href="http://www.alidays.it/" target="_blank">Alidays</a>, a travel company always striving to improve their experience through technology.</h3>
             <h3>This is an internal tool case study for a platform developed to speed up and aid the employees of the company in their daily tasks.</h3>
         </div>
         </div>
         </div>
     <div className="Project-Body">
-    <p>The company needed  an internal tool that could help ease the load of requests and overall workload of the employees.<br></br>So, we had to find a way to speed up the process where it was possible: one shot ticket purchases (i.e. hotels and flights).</p>
+      <h3>The Problem</h3>
+      <p>The company needed  an internal tool that could help ease the load of requests and overall workload of the employees.<br></br>So, we had to find a way to speed up the process where it was possible: one shot ticket purchases (i.e. hotels and flights).</p>
     <p> We started off defining what were the needs of our users-the employees (by detailing their task flows on the existing platform and finding their pain points) and by taking a closer look at the tools used, figuring out where they could be improved and what needed to be kept to avoid a complete detachment of the new tool.</p>
-    <img src="../images/alidays-fast1.jpg"></img>
-    <h3>THE LIMITATIONS</h3>
+    <h3>Users and audience</h3>
+    <p>Primary and secondary teachers and - indirectly their pupils. It is interesting to notice how - even though the subscription was aimed at teachers - research showed us how they responded better to a branding and visual language that was in line with the need of their students (more colourful and playful), as a proxy of what they could expect from the resources themselves.</p>
+    <h3>My role</h3>
+    <p>I acted as the product designer for this internal tool: I talked to the stakeholders along with the CTO before starting the project, collected insights and interviews from the users, did the initial analysis along with the developer, pen + paper wireframes, clickable prototype and made  sure that development was done according to both requirements and design.</p>
+    <h3>Constraints and Limitations</h3>
     <p>The biggest limitation had to be the time frame in which we could bring this new tool to life. We also had to stay focused on both giving a clean, lean UX experience while delivering all the info and flexibility the users would need.</p>
     <img src="../images/alidays-fast2.jpg"></img>
-    <h3>THE SOLUTION</h3>
+    <h6>Detail interaction of the navigation.</h6>
+    <h3>The Solution</h3>
     <p>We found the answer by giving this tool a clear purpose and identity as a product.<br></br>The most relevant problem we had encountered while testing with the final users was the need of flexibility they had. 
     So, we turned this tool into a feature of the bigger scope backoffice platform, instead of a tool in itself.</p>
     <img src="../images/alidays-fast3.jpg"></img>
-    <h3>MY ROLE</h3>
-    <p>I acted as the product designer for this internal tool: I talked to the stakeholders along with the CTO before starting the project, collected insights and interviews from the users, did the initial analysis along with the developer, pen + paper wireframes, clickable prototype and made  sure that development was done according to both requirements and design.</p>
     <h6>DUE TO THE NATURE OF THE PROJECT, SENSITIVE DETAILS ARE WITHHELD IN ORDER TO PROTECT THE UNDERLYING INTELLECTUAL PROPERTY OF THE PRODUCT.</h6>
     </div>
   </Layout>
--- src/pages/soldo-app.js
+++ src/pages/soldo-app.js
@@ -12,22 +12,20 @@ const ProjectPage = () => (
       rel="stylesheet">
       </link>
     <a href="/"><i class="material-icons md-48" >arrow_back</i></a>
-            <h2>Mobile app</h2>
-            <h3>Soldo Business is a payment platform that allows companies to control more efficiently their employees' spending.</h3>
+      <h2>Soldo Mobile App</h2>
+            <h3>Soldo Business is a payment platform that allows companies to control more efficiently their employees' spending.<br></br></h3>
         </div>
         </div>
         </div>
     <div className="Project-Body">
-    <p>Soldo is a multi-user spending account for business and families which allows total control over spending and budgeting tasks via the mobile apps.
-I worked on designing and improving a number of features for the consumer and business facing mobile apps by:
+      
+      <p>I worked on designing and improving a number of features for the consumer and business facing mobile apps by
 Creating UX/UI designs and clear documentation to support</p>
-<p>Owning the user experience and taking ownership of one or more parts of the user experience: mapping user journeys, writing user stories, designing interactions, present and defend designs to other team members and executive level stakeholders
+<p>Owning the user experience and taking ownership of presenting and collaborating with the various stakeholders: mapping user journeys, writing user stories, designing interactions, present and defend designs to other team members and executive level stakeholders
 Owning feature development  and guiding the team to the development of the designed user experience by interacting with the developers and fellow designers.</p>
     <img src="../images/soldo-mobile.png"></img>
-    </div>
+</div>
   </Layout>
 )
-
-
 
 export default ProjectPage
